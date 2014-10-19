45be2aabf751a337bdfad85af2987cd8bc5267d8
n9Mtq4 <bill.wresnahan@gmail.com>
2014-10-19 14:17:47 -0400

    Added a console parser(WIP)
    This will be used in the future for parting the console input.

---
+++ src/com/n9mtq4/math/sysin/ConsoleParser.java
@@ -0,0 +1,20 @@
+package com.n9mtq4.math.sysin;
+
+/**
+ * Created by Will on 10/19/14.
+ */
+public class ConsoleParser {
+	
+	private Console parent;
+	
+	public ConsoleParser(Console parent) {
+		this.parent = parent;
+	}
+	
+	public void push(String text) {
+		
+		
+		
+	}
+	
+}
